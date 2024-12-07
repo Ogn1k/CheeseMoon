7f96c3a8170ac1c6eb9b17bcc883258d5a03ab40
Ogn1k <62839671+Ogn1k@users.noreply.github.com>
2024-12-07 11:50:15 +0500

    fix movement
    TODO: fix movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -69,7 +69,7 @@ public class PlayerMovementTutorial : MonoBehaviour
         verticalInput = Input.GetAxisRaw("Vertical");
 
         // when to jump
-        if (Input.GetKey(jumpKey) && readyToJump && grounded)
+        if (Input.GetKeyDown(jumpKey) && readyToJump && grounded)
         {
             readyToJump = false;
 
